b18965b5911210903b5292af8ebe5b71fced27a2
Andres Felipe <91640921+AndresGratus@users.noreply.github.com>
2022-04-22 19:57:45 -0500

    Creamos la clase juego y Agregamos los arr[]

---
+++ src/laboratorisofkajava/Juego.java
@@ -0,0 +1,22 @@
+
+package laboratorisofkajava;
+
+
+public class Juego {
+    //Creamos los arreglos para las preguntas
+    
+    private Pregunta arrUno[];
+    private Pregunta arrDos[];
+    private Pregunta arrTres[];
+    private Pregunta arrCuatro[];
+    private Pregunta arrCinco[];
+    
+    //Metodo Juego inicializar los arreglos
+    
+    public void Juego(){
+        Pregunta arrUno[] ={
+        new Pregunta("¿Cuál es el nombre del río más largo del mundo?","Río Nilo","Río Amazonas","Río Danubio","Río Uruguay","optionB")
+        };
+    }
+    
+}
